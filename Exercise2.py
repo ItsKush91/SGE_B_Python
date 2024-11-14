654bf8e1d5c028f49e9a703f254da3502f0aca0f
Kushal Lamichhane <Kushal9765@gmail.com>
2024-11-14 11:57:17 +0100

    1-4 completed

---
+++ Exercise2.py
@@ -0,0 +1,19 @@
+print("Enter a Day")
+day = input()
+
+if day == "Dilluns":
+    print("Avui és",day)
+elif day == "Dimarts":
+    print("Avui és",day)
+elif day == "Dimecres":
+    print("Avui és",day)
+elif day == "Dijous":
+    print("Avui és",day)
+elif day == "Divendres":
+    print("Avui és",day)
+elif day == "Dissabte":
+    print("Avui és",day)
+elif day == "Diumenge":
+    print("Avui és",day)
+else:
+    print("Invalid day")
